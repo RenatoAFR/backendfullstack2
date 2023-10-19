daa4d6d26fe9ee28c7d989930615218d78b431d5
RenatoAF <renato012br@hotmail.com>
2023-10-18 21:26:45 -0300

    fix: senha novamente

--- Persistencia/Conexao.js
+++ Persistencia/Conexao.js
@@ -8,7 +8,7 @@ export default async function Conectar() {
     const conexao = await mysql.createConnection({
         host: "localhost",
         user: "aluno38-pfsii",
-        password: "vhDcIp1yrpfTjAJnvt9B",
+        password: "6fSrhL1PuHaDJ606pT3n",
         database: "renato"
     });
 
